af8b51939ca635f2b53e17701bc2c860f2178f2c
Woodward <JDWoodward@cps.k12.il.us>
2025-10-20 13:36:22 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,11 @@
+#comments (python)
+#variables are used to store data values
+x = 5       #integer         Normal numbers
+y = 3.14    #float           Decimals
+name = "Joshua" #string      Letters in quotations
+is_student = True #boolean   True or false binary statements
+#Print values of variables
+print(x)
+print(y)
+print(name)
+print(is_student)
---
+++ python_basics/lesson1.py
@@ -0,0 +1,27 @@
+#comments (python)
+#variables are used to store data values
+x = 5       #integer         Normal numbers
+y = 3.14    #float           Decimals
+name = "Joshua" #string      Letters in quotations
+is_student = True #boolean   True or false binary statements
+#print values of variables
+print(x)
+print(y)
+print(name)
+print(is_student)
+print("My name is " + name + ", I am " + str(x) + " years old")
+#the plus sign (+) is used to concatenate strings
+print(type(x))          #<class 'int'>
+print(type(y))          #<class 'float'>
+print(type(name))       #<class 'str'>
+print(type(is_student)) #<class 'bool'>
+#lets make a new set of variables to practice
+a = 10
+b = 2.5
+c = "Bob"
+d = False 
+#print the values of the new variables in a sentence 
+#using concatenation
+print("His name is" + c + ", he is " + a + " years old and the rumors about him are " + d + ", over his " + b + "years of grade school")
+#using the f string method
+print(f"My name is {c}, I have {a} appless.")
